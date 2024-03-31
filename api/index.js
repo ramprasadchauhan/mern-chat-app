5a20bacf38733766d91cc84839e4d7ac2dbd5091
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-31 21:49:35 +0530

    create message, conversation model. Send message endpoint and make protected route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5100;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(PORT, () => {
   connectToMongoDB();
