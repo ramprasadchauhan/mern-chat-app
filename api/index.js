f698cf0699b5905a3539691954ea5ac590834968
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-31 22:34:52 +0530

    get All mesage and get filter user in sidebar

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import userRoute from "./routes/user.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   connectToMongoDB();
