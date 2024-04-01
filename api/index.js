f2d55f5166ed3aeedfb3d2e87cc589cb9f3d6cdb
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-04-01 18:44:03 +0530

    app completed

--- api/index.js
+++ api/index.js
@@ -5,9 +5,12 @@ import messageRoute from "./routes/message.route.js";
 import userRoute from "./routes/user.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 5100;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,6 +19,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server is running on port ${PORT}`);
