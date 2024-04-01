97582c6d749161f999f4cada649a0a0e1304bff3
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-04-01 15:55:13 +0530

    send message and get message functionality completed

--- client/src/components/messages/MessageInput.jsx
+++ client/src/components/messages/MessageInput.jsx
@@ -1,19 +1,35 @@
 import { BiSolidSend } from "react-icons/bi";
+import useSendMessage from "../../hooks/useSendMessage";
+import { useState } from "react";
 
 const MessageInput = () => {
+  const { loading, sendMessage } = useSendMessage();
+  const [message, setMessage] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
+          value={message}
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
           placeholder="Send a message"
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3"
         >
-          <BiSolidSend />
+          {loading ? (
+            <div className="loading loading-spinner"></div>
+          ) : (
+            <BiSolidSend />
+          )}
         </button>
       </div>
     </form>
--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -1,8 +1,20 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emogis";
 import Conversation from "./Conversation";
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log("Conversations", conversations);
   return (
     <div>
-      <Conversation />
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
+      {loading ? <span className="loadind loading-spinner"></span> : null}
     </div>
   );
 };
